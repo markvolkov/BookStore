8ff1737cd74fe8a29b0bb5cad6fa90f90565d046
Jacquelyn Martin <jacquelynmartin@Jacquelyns-MacBook-Air.local>
2017-12-05 11:55:51 -0500

    Added LCommand

---
+++ commands/impl/LCommand.h
@@ -0,0 +1,30 @@
+
+// Created by Jacquelyn Martin on 12/5/17.
+//
+
+#ifndef BOOKSTORE_LCOMMAND_H
+    #define BOOKSTORE_LCOMMAND_H
+
+#include "../Command.h"
+
+class LCommand : public Command {
+private:
+    std::string identifier;
+public:
+    LCommand();
+    LCommand(std::string identifier);
+
+    std::string getName() override;
+
+    std::string toString() override;
+
+    void execute(std::string args[]) override;
+
+};
+
+
+#endif //BOOKSTORE_LCOMMAND_H
+
+
+
+
