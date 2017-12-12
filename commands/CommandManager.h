da41bfd687f8898d918123246d65e3fb76b456e0
Mark Volkov <markvolkov@Marks-MacBook-Pro.local>
2017-12-12 11:20:26 -0500

    Added CommandManager

---
+++ commands/CommandManager.h
@@ -0,0 +1,26 @@
+//
+// Created by Mark Volkov on 11/28/17.
+//
+using namespace std;
+
+#ifndef BOOKSTORE_COMMANDMANAGER_H
+#define BOOKSTORE_COMMANDMANAGER_H
+
+#include "Command.h"
+#include "../dstrctures/map/Map.h"
+
+class CommandManager {
+
+private:
+    Map<string, Command*>* commands;
+public:
+    CommandManager();
+    ~CommandManager();
+    Command* getCommand(string identifer);
+    bool addCommand(Command* command);
+    void executeCommand(string identifier, string args[]);
+    void loadCommands();
+};
+
+
+#endif //BOOKSTORE_COMMANDMANAGER_H
