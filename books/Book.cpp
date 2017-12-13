c9dbf324099a57f68af2dd7eec6d26ffd7eb1dcc
Jacquelyn Martin <jacquelynmartin@Jacquelyns-MacBook-Air.local>
2017-12-13 17:40:31 -0500

    books.cpp and .h

--- books/Book.cpp
+++ books/Book.cpp
@@ -3,5 +3,38 @@
 //
 
 #include "Book.h"
+#include "Customer.h"
+#include "LinkedQueue.h"
+
+Book::Book(std::string title, std::string author, int stockCount, int wishCount){
+    this->title = title;
+    this->author = author;
+    this->stockCount = stockCount;
+    this->wishCount = wishCount;
+    LinkedQueue* waitlist = new LinkedQueue;
+}
 
 
+std::string Book::getTitle() { return this->title; }
+
+std::string Book::getAuthor() { return this->author; }
+
+int getStockCount(){ return this->stockCount; }
+
+int getWishCount(){ return this->wishCount; }
+
+void addToStockCount(int numCopies){
+    numCopies += numCopies;
+}
+
+void removeFromStockCount(int numCopies){
+    numCopies -= numCopies;
+}
+
+void enqueueWaitlist(Customer* customer){
+    enqueueWaitlist(customer);
+}
+
+Customer* dequeueWaitlist(){
+   return dequeueWaitlist();
+}
--- books/Book.h
+++ books/Book.h
@@ -11,9 +11,22 @@ private:
     std::string title;
     //When stock < wish order more books.
     //When sock > wish return some books.
+    std::string author;
     int stockCount;
     int wishCount;
+    LinkedQueue* waitlist;
 
+public:
+    Book(std::string title, std::string author, int stockCount, int wishCount);
+    std::string getTitle();
+    std::string getAuthor();
+    int getStockCount();
+    int getWishCount();
+
+    void addToStockCount(int numCopies);
+    void removeFromStockCount(int numCopies);
+    void enqueueWaitlist(Customer* customer);
+    Customer* dequeueWaitlist();
 
 };
 
